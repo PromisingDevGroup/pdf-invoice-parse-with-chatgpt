a203389d7b7a39676b869d78cd7c0311a1506ada
Power-Howdy <giftedclan0305@gmail.com>
2023-07-17 10:34:30 +0200

    config revise

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  distDir: '.next',
-  cache: {
-    // Set cache directory to a custom location
-    cacheDirectory: '/build_cache'
-  }
+  // output: "export",
+  // distDir: '.next',
+  // cache: {
+  //   // Set cache directory to a custom location
+  //   cacheDirectory: '/build_cache'
+  // }
 }
 
 module.exports = nextConfig
